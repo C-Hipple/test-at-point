28da7d00ef1383e72d8ad19390407576be0479a8
Chris <hipplec@Chriss-MacBook-Pro.local>
2024-10-10 22:06:30 -0400

    create project

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # test-at-point
 Emacs Package for easily sending unit tests to compile-mode
+
+
+This package adds some simple functions for allowing you to quickly run individual unit tests across a variety of major-modes using compile mode.
+
+
+## Usage
+
+Calling `run-test-at-point` will lookup the test at the current point via regular expression searching and then run that only that test via `compile-mode`.
+
+Add your preferred keybinding to your init.
+
+```elisp
+(define-key evil-normal-state-map (kbd "SPC c t") 'run-test-at-point)
+```
+
+## Installation
+
+Use your favorite package manager to install from this github repository
+
+### Spacemacs
+
+(I use spacemacs, but any emacs should work)
+
+```elisp
+dotspacemacs-additional-packages '(
+  ...
+  (code-review :location (recipe
+    :fetcher github
+    :repo "C-Hipple/test-at-point"
+    :files ("*.el")))
+  ...
+)
+```
+
+
+## Extending functionality
+
+Currently only python, go, and rust are supported.  To add more languages add values do the following:
+1. Add the regex which finds a test function name to the alist `mode-test-pattern-alist`
+2. Add a command builder function and link it to the major mode via `mode-command-pattern-alist`
+
+
+## Setting custom test command functions
+
+A lot of larger projects will have custom tooling or specific commands which need to be used in order to run tests.  
+
+In the below example we override the rust test command builder to always use the environment variable as RUST_BACKTRACE=1.
+
+
+```elisp
+
+(defun diff-lsp-test-command (file-name test-name)
+  (concat "RUST_BACKTRACE=1 cargo test " test-name))
+
+(setq project-mode-command-overide-alist
+      ;; example with one of my other open source projects
+      ;; Setting various rust modes depending on how that emacs is configured.
+      '(("diff-lsp" . ((rustic-mode . diff-lsp-test-command),
+                       (rust-mode . diff-lsp-test-command),
+                       (rust-ts-mode . diff-lsp-test-command)))))
+```
+
+
---
+++ test-at-point.el
@@ -0,0 +1,91 @@
+(defun go-test-command (file-name test-name)
+  ;;go test -v -run Test_Serialize
+  (concat "go test -v -run " test-name))
+
+(defun py-test-command (file-name test-name)
+  ;;pytest test_main.py::test_add
+  ;;pytest -k test_add
+  (concat "pytest -k " test-name))
+
+(defun rust-test-command (file-name test-name)
+  (concat "cargo test " test-name))
+
+(defun diff-lsp-test-command (file-name test-name)
+  (concat "RUST_BACKTRACE=1 cargo test " test-name))
+
+(setq mode-command-pattern-alist
+      '((go-mode . go-test-command)
+        (go-ts-mode . go-test-command)
+        (python-mode . py-test-command)
+        (python-ts-mode . py-test-command)
+        (rust-ts-mode . rust-test-command)
+        (rust-mode . rust-test-command)
+        (rustic-mode . rust-test-command)))
+
+
+(setq project-mode-command-overide-alist
+      ;; example with one of my other open source projects
+      ;; Setting various rust modes depending on how that emacs is configured.
+      '(("diff-lsp" . ((rustic-mode . diff-lsp-test-command),
+                       (rust-mode . diff-lsp-test-command),
+                       (rust-ts-mode . diff-lsp-test-command)))))
+
+
+(defun run-test-at-point ()
+  "Runs the test command based on major mode and test name."
+  (interactive)
+  (let* ((mode-command (cdr (assoc major-mode mode-command-pattern-alist)))
+         (project-overides (cdr (assoc (projectile-project-name) project-mode-command-overide-alist))))
+    (if project-overides
+        (compile (funcall (cdr (assoc major-mode project-overides)) (buffer-file-name) (current-test-at-point)))
+      (if mode-command
+          (compile (funcall mode-command (buffer-file-name) (current-test-at-point))))
+      (message "No command found for %s mode" major-mode))))
+
+
+(setq mode-test-pattern-alist
+      '(
+        (go-mode . "^func \\(Test_[a-zA-Z0-9_+]+\\)")
+        (go-ts-mode . "^func \\(Test_[a-zA-Z0-9_+]+\\)")
+        (python-mode . "^def \\([a-zA-Z0-9_]+\\)")
+        (python-ts-mode . "^def \\([a-zA-Z0-9_]+\\)")
+        (rust-mode . "fn \\(test_[a-zA-Z0-9_+]+\\)")
+        (rust-ts-mode . "fn \\(test_[a-zA-Z0-9_+]+\\)")
+        (rustic-mode . "fn \\(test_[a-zA-Z0-9_+]+\\)")))
+
+(defun get-pattern-by-mode ()
+  (interactive)
+  (let ((mode-pattern (assoc major-mode mode-test-pattern-alist)))
+    (if mode-pattern
+        (cdr mode-pattern))))
+
+
+(defun current-test-at-point ()
+  (let ((my-line (thing-at-point 'line))
+        (pattern (get-pattern-by-mode))
+        (result nil))
+    (if (string-match pattern my-line)
+        (setq result (match-string 1 my-line))
+      (save-excursion
+        (while (and (re-search-backward pattern nil t 1) (not result))
+          (beginning-of-line)
+          (let ((this-line (thing-at-point 'line)))
+            (when (string-match pattern this-line)
+              (setq result (match-string 1 this-line)))))))
+    result))
+
+
+(defun call-current-test-at-point ()
+  ;; lil helper to debug
+  (interactive)
+  (let ((res (current-test-at-point)))
+    (message (concat "Found above test: " res ))))
+
+(defun call-get-pattern-by-mode()
+  (interactive)
+  (let ((res (get-pattern-by-mode)))
+    (message (concat "Found the pattern: " res))
+    )
+  )
+
+;; hotkeys
