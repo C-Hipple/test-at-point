e4d1a0f3b4e823c1939ccbf56f680be3c7db3c1a
Chris Hipple <chrishipple@multimediallc.com>
2025-04-04 16:49:27 -0400

    remove source subdir

--- docs/conf.py
+++ docs/conf.py
@@ -1,28 +1,35 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-project = 'test-at-point'
-copyright = '2024, Chris Hipple'
-author = 'Chris Hipple'
+# -- Project information
+
+project = 'test-at-point.el'
+copyright = '2024+ Hipple'
+author = 'C-Hipple'
+
 release = '0.1'
+version = '0.1.0'
 
-# -- General configuration ---------------------------------------------------
+# -- General configuration
+
 extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
 ]
 
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# -- Options for HTML output -------------------------------------------------
+# -- Options for HTML output
+
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
 
-# -- Options for autodoc extension -------------------------------------------
-autodoc_member_order = 'bysource'
-autodoc_typehints = 'description'
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
